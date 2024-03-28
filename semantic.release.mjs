4c1b5aa7d6c0edbf822bbd8fe3dc189924a1e992
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-03-28 14:04:34 +0100

    ci: configure workflow

---
+++ semantic.release.mjs
@@ -0,0 +1,26 @@
+process.env.SEMANTIC_RELEASE_PACKAGE = "Eslint plugin Ts.ED";
+
+export default {
+  branch: 'main',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          'package.json',
+          'yarn.lock'
+        ]
+      }
+    ],
+    '@semantic-release/npm',
+    [
+      'semantic-release-slack-bot',
+      {
+        markdownReleaseNotes: true,
+        notifyOnSuccess: true
+      }
+    ]
+  ]
+}
